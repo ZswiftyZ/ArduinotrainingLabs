2cc57119fb7cca70b190d167bfca8c1c55f8f955
Trenton Weller <trenton.weller1298@gmail.com>
2019-11-14 13:42:43 -0500

    updatedtime_led

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -23,7 +23,7 @@ pinMode(led, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(led, HIGH);
-delay(1000);
+delay(1500);
 digitalWrite(led, LOW );
-delay(1000);
+delay(1500);
 }
