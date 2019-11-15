c99680d8ca19d0152f346daba534bbea9d1cd7ed
Trenton Weller <trenton.weller1298@gmail.com>
2019-11-15 14:03:08 -0500

    initial Commit

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,29 +1,30 @@
 /*Programmer: Trenton Weller
-*Date: 11.6.19
-*Program: Blink
+*Date: 11.14.19
+*Program: digitalRead() & the Serial Port
 *
-*This program will turn on an LED for specified amounts of tiume, turn it
-*off, and back on in a while Loop
+*Reads a digital input on pin2, prints the result to the serial monitor
 *
 */
 
-// initialize our led to PIN
-int led = 13;
 
-
-
-
-
-
+//digital pn2 has a push button attached to it
+int pushButton = 2;
 void setup() {
-  // Using the Function pinMode to call led and provide output
-pinMode(led, OUTPUT);
+  //initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+  //make the pushButton an input
+  pinMode(pushButton, INPUT);
+
+  
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(led, HIGH);
-delay(1500);
-digitalWrite(led, LOW );
-delay(1500);
+  //read the input pin
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button
+  Serial.println(buttonState);
+  delay(250); // delay in between readings for stability
+
+  
 }
