17b06f83d0bfb066fd6c401a01a3c17de785fa7f
Trenton Weller <trenton.weller1298@gmail.com>
2019-12-03 14:07:22 -0500

    Initial Potentiomoter!

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,9 +1,8 @@
 /*Programmer: Trenton Weller
-*Date: 11.6.19
-*Program: Blink
+*Date: 12.3.19
+*Program: Potentiometer
 *
-*This program will turn on an LED for specified amounts of tiume, turn it
-*off, and back on in a while Loop
+*Reads an anolog input on pin A0, prints to the serial monitor
 *
 */
 
@@ -11,23 +10,10 @@
 int led = 13;
 int pushButton =2;
 void setup()  {
-   pinMode(led,OUTPUT);
-   Serial.begin(9600);
-   pinMode(pushButton,INPUT);
+   
 }
 
 void loop()  {
-  //read the input pin
-  int buttonState = digitalRead(pushButton);
-   // if buttonstate is true/pushed by human the LED with Blink
-  Serial.println(buttonState);
-  if (buttonState==HIGH){
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led,LOW);
-  }
-  delay(1); // delay in between readings for stability
+  
 }
-
  
